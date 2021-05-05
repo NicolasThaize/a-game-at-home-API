e31777ab4b0e833081e35b31ce9f4f284334c656
Nicolas THAIZE <nicolas.thaize@ynov.com>
2021-05-05 19:10:48 +0200

    Adding blacklist endpoint

--- MyProject/api_core/urls.py
+++ MyProject/api_core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import UserViewSet,TeamViewSet,ProofViewSet,SessionViewSet,ChallengeViewSet
+from .views import UserViewSet,TeamViewSet,ProofViewSet,SessionViewSet,ChallengeViewSet,LogoutAndBlacklistRefreshTokenForUserView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt import views as jwt_views
 from .views import ObtainTokenPairWithInfosView
@@ -20,4 +20,5 @@ urlpatterns = [
     path('',include(router.urls)),
     path('token/obtain/', ObtainTokenPairWithInfosView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('blacklist/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='blacklist')
 ]
