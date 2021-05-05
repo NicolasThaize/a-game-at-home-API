00e1012ac93f2b12cc5b7f34ead400bc653c8188
Nicolas THAIZE <nicolas.thaize@ynov.com>
2021-05-05 14:30:36 +0200

    More infos in jwt

--- MyProject/api_core/serializers.py
+++ MyProject/api_core/serializers.py
@@ -1,6 +1,26 @@
 from rest_framework import serializers
 from .models import CustomUser,Team,Proof,Session,Challenge
 
+# Auth related:
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+# Adding all needed informations to token
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+        # Add custom fields
+        token['username'] = user.username
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['email'] = user.email
+
+        return token
+
+
+
+###################################################################################
 class UserSerializer(serializers.ModelSerializer):
     birthDate = serializers.DateField(input_formats=['%d-%m-%Y',])
     class Meta:
